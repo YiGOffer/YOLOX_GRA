d1a935d9074045044b17560c40081904837f4af1
YiGOffer <yyjshang@163.com>
2023-05-27 19:32:52 +0800

    sheep  person

--- get_map.py
+++ get_map.py
@@ -89,7 +89,8 @@ if __name__ == "__main__":
                     obj_name = obj.find('name').text
                     if obj_name not in class_names:
                         continue
-                    if obj_name not in ["person", "cow", "horse"]:
+                    # if obj_name not in ["person", "cow", "horse"]:
+                    if obj_name not in ["person", "sheep"]:
                         continue
                     bndbox  = obj.find('bndbox')
                     left    = bndbox.find('xmin').text
